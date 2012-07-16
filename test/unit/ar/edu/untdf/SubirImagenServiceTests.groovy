2a9a20cb50a3dadbe6af93aafdb55470be886c9d
Alejandro <Alejandro@Alejandro-PC>
2012-07-16 17:30:19 -0300

    Carga archivo, error al eliminar obra (a veces)

---
+++ test/unit/ar/edu/untdf/SubirImagenServiceTests.groovy
@@ -0,0 +1,17 @@
+package ar.edu.untdf
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(SubirImagenService)
+class SubirImagenServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
